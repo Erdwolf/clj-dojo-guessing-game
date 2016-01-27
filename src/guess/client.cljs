c2dbc0efc2b526bbeea47a5b8c030c5fd5f7a057
Matthias Reinarz <mreinarz@thoughtworks.com>
2016-01-27 15:15:31 +0000

    Update guess on client side (No c/s communication yet)

--- src/guess/client.cljs
+++ src/guess/client.cljs
@@ -3,10 +3,18 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce message (atom "I'm thinking of a number!"))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(def make-a-guess
+  #(reset! message (str "The number is not "
+                        (-> % .-target .-value)
+                        "!")))
 
-(reagent/render-component [hello-world]
+(defn guess-form []
+  [:section
+    [:h1 @message]
+    [:p "Enter your guess:"]
+    [:input {:on-change make-a-guess}]])
+
+(reagent/render-component [guess-form]
                           (. js/document (getElementById "app")))
